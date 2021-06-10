fe14773256097c290555118b892260ad4509f883
Zaharia Constantin <layout.webdesign@gmail.com>
2021-06-10 19:50:35 +0300

    Changes in settings_example.py default file

--- gpsd_influxdb/settings_example.py
+++ gpsd_influxdb/settings_example.py
@@ -14,23 +14,38 @@
 #  You should have received a copy of the GNU General Public License
 #  along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-DEBUG = False
+DEBUG = True
 
-INFLUXDB_HOST = 'localhost'
-INFLUXDB_PORT = '8086'
+INFLUXDB_20v: bool = False
 
-INFLUXDB_USER = None
-INFLUXDB_PASSWORD = None
+INFLUXDB_HOST: str = 'localhost'
+INFLUXDB_PORT: int = 8086
 
+INFLUXDB_USER: str = ''
+INFLUXDB_PASSWORD: str = ''
 
-INFLUXDB_DATABASE = 'gpsd'
+INFLUXDB_TOKEN: str = '' if INFLUXDB_20v else f'{INFLUXDB_USER}:{INFLUXDB_PASSWORD}' \
+    if all([INFLUXDB_USER, INFLUXDB_PASSWORD]) else ''
+
+INFLUXDB_ORGANIZATION: str = '-'
+
+INFLUXDB_DATABASE: str = 'ntp_server'
+INFLUXDB_RETENTION_POLICY: str = 'autogen'
+INFLUXDB_MEASURMENT: str = 'gpsd-python'
+
+INFLUXDB_BUCKET: str = '' if INFLUXDB_20v else f'{INFLUXDB_DATABASE}/{INFLUXDB_RETENTION_POLICY}'
+
+INFLUXDB_SSL: bool = False
+INFLUXDB_CERT_PATH: str = ''
+
+INFLUXDB_URL: str = '{}://{}:{}'.format('https' if INFLUXDB_SSL else 'http', INFLUXDB_HOST, INFLUXDB_PORT)
 
 #############################
 # Global settings           #
 #############################
 
 # update interval for each push to influxDB
-SCHEDULE_UPDATE_INTERVAL = 30
+SCHEDULE_UPDATE_INTERVAL: int = 30
 
 ###########
 # LOGGING #
